61a3be24d9f0fa25580006ae4882a5e513ff9db9
tarcisioemanoel15 <caninosbrancos10031@gmail.com>
2021-10-18 23:24:34 -0200

    cronometrocomit

--- TarJS/1 cronometro/cro.js
+++ TarJS/1 cronometro/cro.js
@@ -1,1 +1,53 @@
-alert('Mais uma tentativa praticando');
+
+// document.querySelector('body').style.background = 'red'
+// document.querySelector('section').style.background = 'blue'
+
+function Cronometro(){
+    this.inicia = () =>  {
+        document.addEventListener('click', e => {
+            let el = e.target;
+            if(el.classList.contains('iniciar')){
+                cron.classList.add('tr')
+                clearInterval(timer);
+                contador(segundos);
+            }
+                        
+            if(el.classList.contains('pausar')){
+                cron.classList.add('t')
+                cron.classList.remove('tr')
+                clearInterval(timer);
+            }
+                        
+            if(el.classList.contains('zerar')){
+                cron.classList.remove('t')
+                cron.classList.remove('tr')
+                
+                segundos = 0;
+                cron.innerHTML = '00:00:00';
+            }
+        })
+    }
+            
+    let cron = document.querySelector('.contador');
+    this.seg = (segundos) => {
+        let data = new Date(segundos * 1000);
+        return data.toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'UTC'
+        })
+    }
+    console.log(this.seg(10));
+    
+    let segundos = 0;
+    let timer;
+    
+    contador = () => {
+        timer = setInterval(() => {
+            segundos++;
+            cron.innerHTML = this.seg(segundos);
+        }, 1000)
+    }
+}
+
+const cro = new Cronometro();
+cro.inicia();
